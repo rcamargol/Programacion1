b5554e7e7c7e9a9d33e6ac4b2d45fe86a5f80549
rcamargol <rcamargol@unbosque.edu.co>
2018-08-17 12:51:41 -0500

    cambios menores

--- PlantillaMC/src/Controlador/Principal.java
+++ PlantillaMC/src/Controlador/Principal.java
@@ -2,12 +2,10 @@ package Controlador;
 
 public class Principal {
 
-	
 	public static void main(String[] args) 
 	{
 		//Esta sería la unica instrucción que va aqui
 		Controlador c = new Controlador();
 	}
-	
 
 }
