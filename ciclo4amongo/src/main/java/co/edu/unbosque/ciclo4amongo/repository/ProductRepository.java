e797c7726f5628666cced774828758166b595c19
rcamargol <rcamargol@unbosque.edu.co>
2021-11-03 20:55:02 -0500

    Prueba.

--- ciclo4amongo/src/main/java/co/edu/unbosque/ciclo4amongo/repository/ProductRepository.java
+++ ciclo4amongo/src/main/java/co/edu/unbosque/ciclo4amongo/repository/ProductRepository.java
@@ -3,7 +3,7 @@ package co.edu.unbosque.ciclo4amongo.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import co.edu.unbosque.ciclo4amongo.model.Product;
-
+//Este es un comentario re chimbo
 public interface ProductRepository extends MongoRepository < Product, Long > {
 
 }
